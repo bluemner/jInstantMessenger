f594aefc72b234dc1bc0b55367c4aa36e51888c9
Bluemner <Bluemner@uwm.edu>
2014-06-04 20:36:21 -0500

    + Client + Server
    Signed-off-by: Bluemner <Bluemner@uwm.edu>

---
+++ jInstantMessenger/src/com/messenger/client/Client.java
@@ -0,0 +1,18 @@
+package com.messenger.client;
+
+/**
+ * 
+ * @author Beta
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
