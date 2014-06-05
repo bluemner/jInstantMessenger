57c67054fa1c48c3c0a09eb3b039b9835a84977d
Bluemner <Bluemner@uwm.edu>
2014-06-04 21:00:51 -0500

    Added License
    Signed-off-by: Bluemner <Bluemner@uwm.edu>

--- jInstantMessenger/src/com/messenger/client/Client.java
+++ jInstantMessenger/src/com/messenger/client/Client.java
@@ -1,12 +1,26 @@
 package com.messenger.client;
+/*
+ Copyright [2014] [Brandon Bluemner]
 
-/**
- * 
- * @author Beta
- *d
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ 
+ */
+ /**
+ * @version 1.0.0.0
+ * @author Brandon Bluemner
  */
 public class Client {
-
+ 
 	/**
 	 * @param args
 	 */
@@ -15,4 +29,22 @@ public class Client {
 
 	}
 
+	public Client(String server, int port, String userName, String Password){
+		
+		
+	}
+	
+	/**
+	 * Starts an instance of client
+	 */
+	private void start( ){
+		
+		
+	}
+	private void connect ( ) {
+		
+	}
+	private void disconnect ( ) {
+		
+	}
 }
--- jInstantMessenger/src/com/messenger/server/Server.java
+++ jInstantMessenger/src/com/messenger/server/Server.java
@@ -1,5 +1,25 @@
 package com.messenger.server;
+/*
+Copyright [2014] [Brandon Bluemner]
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+/**
+ * @version
+ * @author Beta
+ *
+ */
 public class Server {
 
 	/**
@@ -9,5 +29,40 @@ public class Server {
 		// TODO Auto-generated method stub
 
 	}
+	int port;
+	boolean keepRunning=true;
+	
+	/**
+	 * Creates an instance of the server
+	 * @param port Bind the Server to this TCP port.
+	 */
+	public Server(int port){
+		this.port=port;
+		
+	}
+	
+	/**
+	 * Starts The Server
+	 */
+	public void startServer( ){
+		Thread t= new Thread(new Runnable( ){
 
+			@Override
+			public void run( ) {
+				// TODO Code for running Here
+				while(Server.this.keepRunning){
+					
+				}
+				
+				
+			}});
+		
+		t.start( );
+	}
+	/**
+	 * Stops The Server
+	 */
+	public void stopServer(){
+		this.keepRunning=false;
+	}
 }
